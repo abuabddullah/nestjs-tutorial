beb03375c12106b127a89289537a8af9ae2dd5e0
asifaowadud <asifaowadud@gmail.com>
2024-10-18 12:35:04 +0600

    basic db integration done without any relations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
+// import { Module, ValidationPipe } from '@nestjs/common';
+// import { UserModule } from './modules/userModules/User.module';
+// import { APP_PIPE } from '@nestjs/core';
+
+// @Module({
+//   imports: [UserModule],
+//   providers: [
+//     {
+//       provide: APP_PIPE,
+//       useClass: ValidationPipe,
+//     },
+//   ],
+// })
+// export class AppModule {}
+
+/* ************ */
+/* v-2 */
 import { Module, ValidationPipe } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './modules/userModules/User.module';
 import { APP_PIPE } from '@nestjs/core';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    MongooseModule.forRoot(`mongodb+srv://asifaowadud:sof6vxfRNfUEvdCg@cluster0.gjcwx8p.mongodb.net/nestjs_tutorial?retryWrites=true&w=majority&appName=Cluster0`), // MongoDB Atlas connection
+    UserModule,
+  ],
   providers: [
     {
       provide: APP_PIPE,
--- src/modules/userModules/User.Controller.ts
+++ src/modules/userModules/User.Controller.ts
@@ -1,59 +1,115 @@
+// import {
+//   Body,
+//   Controller,
+//   Get,
+//   Header,
+//   HttpCode,
+//   HttpStatus,
+//   Param,
+//   ParseIntPipe,
+//   Post,
+//   Query,
+//   Redirect,
+//   Req,
+//   Res,
+// } from '@nestjs/common';
+// import { Request, Response } from 'express';
+// import { UserDataType_DTO } from './User.dto';
+// import { UserService } from './User.Service';
+// import { TrimPipe } from 'src/customPipe/trim.pipe';
+
+// @Controller('users') // http://localhost:3000/users
+// export class UserController {
+//   constructor(private readonly userService: UserService) {}
+//   /* getAllUsers */
+//   @Get()
+//   @Header('Cache-Control', 'none')
+//   getUsers() {
+//     return this.userService.getUsers();
+//   }
+
+//   /* getUserByQuery at query*/
+//   @Get('by-name')
+//   getUserByQuery(@Query('name') name: string) {
+//     console.log('🚀 ~ UserController ~ getUserByQuery ~ name:', name);
+//     return this.userService.getUserByQuery(name);
+//   }
+
+//   /* getUserById at param */
+//   @Get(':id')
+//   getUserById(@Param('id', TrimPipe, ParseIntPipe) id: number) {
+//     return this.userService.getUserById(id);
+//   }
+
+//   /* POST user */
+//   @Post()
+//   @HttpCode(HttpStatus.CREATED)
+//   postUser(@Body() userFromBody: UserDataType_DTO) {
+//     return this.userService.postUser(userFromBody);
+//   }
+
+//   /* get redirected to another route while accessing "/users" */
+//   @Get('/redirectToUsers') // from http://localhost:3000/users/redirectToUsers
+//   @Redirect('/users', 301) // to http://localhost:3000/users
+//   getRedirectToUsers() {
+//     return {
+//       message: 'this is users redirectToUsers route',
+//     };
+//   }
+// }
+
+/* ************ */
+/* v-2 */
+
 import {
   Body,
   Controller,
   Get,
-  Header,
-  HttpCode,
-  HttpStatus,
   Param,
-  ParseIntPipe,
   Post,
+  Delete,
+  Put,
   Query,
-  Redirect,
-  Req,
-  Res,
+  NotFoundException,
+  ParseIntPipe,
 } from '@nestjs/common';
-import { Request, Response } from 'express';
-import { UserDataType_DTO } from './User.dto';
 import { UserService } from './User.Service';
-import { TrimPipe } from 'src/customPipe/trim.pipe';
+import { UserDataType_DTO } from './User.dto';
 
-@Controller('users') // http://localhost:3000/users
+@Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
-  /* getAllUsers */
+
   @Get()
-  @Header('Cache-Control', 'none')
   getUsers() {
     return this.userService.getUsers();
   }
 
-  /* getUserByQuery at query*/
   @Get('by-name')
   getUserByQuery(@Query('name') name: string) {
-    console.log('🚀 ~ UserController ~ getUserByQuery ~ name:', name);
     return this.userService.getUserByQuery(name);
   }
 
-  /* getUserById at param */
   @Get(':id')
-  getUserById(@Param('id', TrimPipe, ParseIntPipe) id: number) {
+  getUserById(@Param('id', ParseIntPipe) id: number) {
     return this.userService.getUserById(id);
   }
 
-  /* POST user */
   @Post()
-  @HttpCode(HttpStatus.CREATED)
-  postUser(@Body() userFromBody: UserDataType_DTO) {
+  createUser(@Body() userFromBody: UserDataType_DTO) {
     return this.userService.postUser(userFromBody);
   }
 
-  /* get redirected to another route while accessing "/users" */
-  @Get('/redirectToUsers') // from http://localhost:3000/users/redirectToUsers
-  @Redirect('/users', 301) // to http://localhost:3000/users
-  getRedirectToUsers() {
-    return {
-      message: 'this is users redirectToUsers route',
-    };
+  @Delete(':id')
+  deleteUser(@Param('id', ParseIntPipe) id: number) {
+    return this.userService.deleteUser(id);
+  }
+
+  @Put(':id')
+  updateUser(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateData: Partial<UserDataType_DTO>,
+  ) {
+    return this.userService.updateUser(id, updateData);
   }
 }
